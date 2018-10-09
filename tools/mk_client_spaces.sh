337ade8e6e60420b0bbb1df1cd3fe095bc72dd64
Andrew Lunde <andrew.lunde@sap.com>
2018-10-09 23:01:31 +0000

    walk dog

--- tools/mk_client_spaces.sh
+++ tools/mk_client_spaces.sh
@@ -36,11 +36,39 @@ while read clientline; do
   hdbsql -i 90 -n localhost:39013 -u SYSTEM -p $password "CREATE USER $sqluser PASSWORD $password NO FORCE_FIRST_PASSWORD_CHANGE SET PARAMETER XS_RC_XS_CONTROLLER_USER = 'XS_CONTROLLER_USER', XS_RC_DEVX_DEVELOPER = 'DEVX_DEVELOPER', XS_RC_XS_AUTHORIZATION_ADMIN = 'XS_AUTHORIZATION_ADMIN'"
 
   xs create-space $spacename -o $orgname
-  xs set-space-role XSA_ADMIN,$sqluser $orgname $spacename SpaceManager
-  xs set-space-role XSA_ADMIN,$sqluser $orgname $spacename SpaceDeveloper
+  xs set-space-role XSA_ADMIN,XSA_DEV,$sqluser $orgname $spacename SpaceManager
+  xs set-space-role XSA_ADMIN,XSA_DEV,$sqluser $orgname $spacename SpaceDeveloper
 
   xs space-users $orgname $spacename
+
+  xs target -o $orgname -s $spacename
+
+  xs create-service hana hdi-shared hdi_db
+#  xs create-service hana hdi-shared hdi_tax_year_2000
 	
+#  xs push $spacename.db -k 1024M -m 256M -p ../db --no-start --no-route
+#  xs bind-service $spacename.db hdi_db
+#  xs restart $spacename.db --wait-indefinitely ; sleep 10 ; xs stop $spacename.db
+
+  hdbsql -i 90 -n localhost:39013 -u SYSTEM -p $password "CREATE SCHEMA $sqluser_1999 OWNED BY $sqluser"
+
+  hdbsql -i 90 -n localhost:39013 -u $sqluser -p $password 'CREATE COLUMN TABLE "'$sqluser'_1999"."basis.contribution"( "contributionId" INTEGER CS_INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1), "partner" NVARCHAR(32) NOT NULL, "amount" DECIMAL(9, 2) CS_FIXED NOT NULL, "created" LONGDATE CS_LONGDATE NOT NULL) UNLOAD PRIORITY 5 AUTO MERGE'
+
+  hdbsql -i 90 -n localhost:39013 -u $sqluser -p $password 'CREATE COLUMN TABLE "'$sqluser'_1999"."basis.withdrawl"( "withdrawlId" INTEGER CS_INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1), "partner" NVARCHAR(32) NOT NULL, "amount" DECIMAL(9, 2) CS_FIXED NOT NULL, "created" LONGDATE CS_LONGDATE NOT NULL) UNLOAD PRIORITY 5 AUTO MERGE'
+
+  hdbsql -i 90 -n localhost:39013 -u $sqluser -p $password 'CREATE COLUMN TABLE "'$sqluser'_1999"."basis.income"( "incomeId" INTEGER CS_INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1), "partner" NVARCHAR(32) NOT NULL, "amount" DECIMAL(9, 2) CS_FIXED NOT NULL, "created" LONGDATE CS_LONGDATE NOT NULL) UNLOAD PRIORITY 5 AUTO MERGE'
+
+  hdbsql -i 90 -n localhost:39013 -u $sqluser -p $password 'CREATE COLUMN TABLE "'$sqluser'_1999"."basis.loss"( "lossId" INTEGER CS_INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1), "partner" NVARCHAR(32) NOT NULL, "amount" DECIMAL(9, 2) CS_FIXED NOT NULL, "created" LONGDATE CS_LONGDATE NOT NULL) UNLOAD PRIORITY 5 AUTO MERGE'
+
+
+  hdbsql -i 90 -n localhost:39013 -u $sqluser -p $password 'INSERT INTO "'$sqluser'_1999"."basis.contribution" VALUES('"'"$spacename"_partner'"',123.45,NOW())'
+
+  hdbsql -i 90 -n localhost:39013 -u $sqluser -p $password 'INSERT INTO "'$sqluser'_1999"."basis.withdrawl" VALUES('"'"$spacename"_partner'"',123.45,NOW())'
+
+  hdbsql -i 90 -n localhost:39013 -u $sqluser -p $password 'INSERT INTO "'$sqluser'_1999"."basis.income" VALUES('"'"$spacename"_partner'"',123.45,NOW())'
+
+  hdbsql -i 90 -n localhost:39013 -u $sqluser -p $password 'INSERT INTO "'$sqluser'_1999"."basis.loss" VALUES('"'"$spacename"_partner'"',123.45,NOW())'
+
   echo ""
 done < $clientfile
 
